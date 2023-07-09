9b638f15e4cbcda236c507178b9505421de3d6e9
realguystuff <101049350+realguystuff@users.noreply.github.com>
2023-07-09 10:00:33 +0000

    some attempts have been made

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 export {};
 
 const express = require('express');
-const path = require('path');
+import path from 'path';
 const favicon = require('serve-favicon');
 
 express().use(favicon(path.join(__dirname,'favicon.ico')));
 
-express().get('/', (req, res) => {
+express().get('/', (req:any, res:any) => {
   res.sendFile('./public/index.html', {root: __dirname});
 });
 
-express().get('/login', (req, res) => {
+express().get('/login', (req:any, res:any) => {
   res.sendFile('./login/index.html', {root: __dirname});
 });
 
-express().get('/signup', (req, res) => {
+express().get('/signup', (req:any, res:any) => {
   res.sendFile('./signup/index.html', {root: __dirname});
 });
 
@@ -22,6 +22,6 @@ express().listen(80, () => {
   console.log('server started on port 80');
 });
 
-express().use(function(req, res) {
+express().use(function(req:any, res:any) {
     res.status(404).end('Error CL5: UnknownPathException (hint: CL5 usually means that you have typed in the path wrong. Check lesmp.tk without any path).');
 });
